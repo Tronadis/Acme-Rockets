6907805da391519f2a16481c260e99cedb931711
Tronadis <89267750+Tronadis@users.noreply.github.com>
2025-03-24 19:54:04 +0100

    Mobile nav menu (basic)

---
+++ 02-acme-rockets/build/js/main.js
@@ -0,0 +1,14 @@
+const initApp = () => {
+	const mobileNavBtn = document.getElementById("mobile-nav-button");
+	const mobileMenu = document.getElementById("mobile-menu");
+
+	const toggleMenu = () => {
+		mobileMenu.classList.toggle("hidden");
+		mobileMenu.classList.toggle("flex");
+	};
+
+	mobileNavBtn.addEventListener("click", toggleMenu);
+	mobileMenu.addEventListener("click", toggleMenu);
+};
+
+document.addEventListener("DOMContentLoaded", initApp);
